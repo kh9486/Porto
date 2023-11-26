ae5841c3129a509ac3699a204b6417fd0d361c59
KangJungA <j112492@naver.com>
2023-11-26 18:02:32 +0900

    feat: 작성 페이지 마무리

--- static/js/script.js
+++ static/js/script.js
@@ -6,3 +6,16 @@ function toggleMenu() {
         menu.style.transform = 'translateX(0%)';
     }
 }
+
+function resizeTextarea() {
+    var elements = document.getElementsByClassName("Experience_details");
+  
+    // 모든 요소에 대해 루프 실행
+    for (var i = 0; i < elements.length; i++) {
+      var textarea = elements[i];
+      textarea.style.height = "auto"; // 높이를 초기화
+  
+      // 실제 높이를 계산하고 설정
+      textarea.style.height = (textarea.scrollHeight) + "px";
+    }
+  }
