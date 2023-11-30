c53445f295c4c131fb08cb462a66d362e6bd5791
KangJungA <j112492@naver.com>
2023-11-30 16:58:43 +0900

    feat: merge conflict 수정

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
+import boto3
+from boto3.dynamodb.conditions import Key
+
 
 app = Flask(__name__)
 
 id_pass = {"admin": "admin"}
+
+aws_access_key_id = 'AKIAWYYZB6Q2Z27JIOKV'
+aws_secret_access_key = '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'
+region_name = 'us-west-2'
+dynamodb_table_name = 'porto_submit'
+
+dynamodb = boto3.resource('dynamodb', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
+table = dynamodb.Table('porto_submit')
+
+
+
+
 
 # 정아야 내가 정말정말 사랑해❤
 
@@ -56,13 +71,99 @@ def register():
     return render_template('register.html')
 
 
+
+
+
+
 @app.route('/resume', methods=['GET', 'POST'])
 def resume():
     if request.method == 'POST':
-        return redirect('/result')
+        return redirect('/resume')
     return render_template('resume.html')
 
 
+
+
+
+@app.route('/submit', methods=['GET','POST'])
+def submit():
+        username = request.form['username']
+        address = request.form['address']
+        phone = request.form['phone']
+        email = request.form['email']
+        Github_url = request.form['Github_url']
+        other_url = request.form['other_url']
+        SKILL1 = request.form['SKILL1']
+        skill_1 = request.form['skill_1']
+        SKILL2 = request.form['SKILL2']
+        skill_2 = request.form['skill_2']
+        SKILL3 = request.form['SKILL3']
+        skill_3 = request.form['skill_3']
+        SKILL4 = request.form['SKILL4']
+        skill_4 = request.form['skill_4']
+        Experience_main_1 = request.form['Experience_main_1']
+        Experience_details_1 = request.form['Experience_details_1']
+        Experience_main_2 = request.form['Experience_main_2']
+        Experience_details_2 = request.form['Experience_details_2']
+        Experience_main_3 = request.form['Experience_main_3']
+        Experience_details_3 = request.form['Experience_details_3']
+
+
+        table.put_item(
+            Item={
+            'username': username,
+            'address': address,
+            'phone': phone,
+            'email': email,
+            'Github_url': Github_url,
+            'other_url': other_url,
+            'SKILL1': SKILL1,
+            'skill_1': skill_1,
+            'SKILL2': SKILL2,
+            'skill_2': skill_2,
+            'SKILL3': SKILL3,
+            'skill_3': skill_3,
+            'SKILL4': SKILL4,
+            'skill_4': skill_4,
+            'Experience_main_1': Experience_main_1,
+            'Experience_details_1': Experience_details_1,
+            'Experience_main_2': Experience_main_2,
+            'Experience_details_2': Experience_details_2,
+            'Experience_main_3': Experience_main_3,
+            'Experience_details_3': Experience_details_3
+                    }
+                        )
+
+
+        return render_template('submit.html',
+        username=username,
+        address=address,
+        phone = phone,
+        email = email,
+        Github_url = Github_url,
+        other_url = other_url,
+        SKILL1 = SKILL1,
+        skill_1 = skill_1,
+        SKILL2 = SKILL2,
+        skill_2 = skill_2,
+        SKILL3 = SKILL3,
+        skill_3 = skill_3,
+        SKILL4 = SKILL4,
+        skill_4 = skill_4,
+        Experience_main_1 = Experience_main_1,
+        Experience_details_1 = Experience_details_1,
+        Experience_main_2 = Experience_main_2,
+        Experience_details_2 = Experience_details_2,
+        Experience_main_3 = Experience_main_3,
+        Experience_details_3 = Experience_details_3
+        )
+
+
+
+
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug="True")
+
 
